f7c2086d3efe62ce2c9562545d810d1a5af4cabb
ArchieDworakowski <archied2003@gmail.com>
2024-08-18 11:56:09 +0100

    Update stegoCrepitus.py
    Cleaned up code

--- stegoCrepitus.py
+++ stegoCrepitus.py
@@ -1,10 +1,9 @@
 import os
 
 os.system("binwalk -Mez *")
-import os
 
 files = os.listdir()
 
 for i in files:
-	if i[0] == "_" and i[1] == "_" and not i == "final":
+	if i[0] == "_" and i[1] == "_":
 		print("possible steganography in " + i)
